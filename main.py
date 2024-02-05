3b157d1e0d54414cd5307b9fff3f6c62112dfc32
James Li <zhiyueli.education@gmail.com>
2024-02-05 10:03:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+import yfinance as yf
+def main():
+    # create ticker for Apple Stock
+    ticker = yf.Ticker('AAPL')
+    # get data of the most recent date
+    todays_data = ticker.history(period='1d')
+
+    print(todays_data)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
