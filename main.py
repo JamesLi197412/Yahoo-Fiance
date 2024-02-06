8fdaf43b7fc77437a646ac022f4724a537e21cdb
James Li <zhiyueli.education@gmail.com>
2024-02-06 20:22:42 +0800

    Update LSTM model and forecast function in the main

--- main.py
+++ main.py
@@ -3,15 +3,23 @@ import yfinance as yf
 from exploration import Exploration
 import warnings
 import pandas as pd
-from model.LSTM import LSTMTagger
+from model.LSTM import StockLSTM
 
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
+
+import numpy as np
+import torch
+import torch.optim as optim
+import torch.utils.data as data
+import torch.nn as nn
+
+
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-def main(stockList):
+def single_stock(stockList):
     # create ticker for Apple Stock
     ticker = yf.Ticker('AAPL')
     df = ticker.history(period='3Y')
@@ -34,13 +42,93 @@ def main(stockList):
     # Go Check signal stationary or not
     ts = df.loc[:,['Close']].copy(deep = True)
     ts = ts.dropna()
-    #ts.index = ts.Date
+    #ts.index = ts.Dates
     #ts = ts.drop("Date", axis = 1)
     # dfexploration.check_adfuller(ts)
     result = dfexploration.test_stationarity(ts,'Close')
     print(f'This Signal is {result}.')
 
+def data_collection():
+    ticker = yf.Ticker('AAPL')
+    df = ticker.history(period = '3Y')
+    return df
 
+def split_data(df, lookback):
+    train_size = int(len(df) * 0.8)
+    test_size = len(df) - train_size
+
+    train,test = df[:train_size], df[train_size :]
+    return train, test
+
+def create_dataset(df,lookback):
+    df = df[['Close']].values
+    X, y = [], []
+    for i in range(len(df) - lookback):
+        feature = df[i:i+lookback]
+        target = df[i+1: i+lookback+1]
+        X.append(feature)
+        y.append(target)
+
+    #return X,y
+    return torch.Tensor(X),torch.Tensor(y)
+
+
+
+def forecast():
+    df = data_collection()
+    ts = df.loc[:, ['Close']].copy(deep=True)
+    ts = ts.dropna()
+    train_size = int(len(ts) * 0.8)
+
+    lookback = 7
+    train, test = split_data(ts, lookback)
+    X_train, y_train = create_dataset(train, lookback = 7)
+    X_test, y_test = create_dataset(test, lookback = 7)
+
+    print('Start Forecasting')
+
+    model = StockLSTM()
+    optimizer = optim.Adam(model.parameters())
+    loss_fn = nn.MSELoss()
+    loader = data.DataLoader(data.TensorDataset(X_train,y_train),shuffle = True, batch_size = 8)
+
+    n_epochs = 2000
+    for epoch in range(n_epochs):
+        model.train()
+        for X_batch, y_batch in loader:
+            y_pred = model(X_batch)
+            loss = loss_fn(y_pred, y_batch)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        # Validation
+        if epoch % 100 != 0:
+            continue
+        model.eval()
+
+        with torch.no_grad():
+            y_pred = model(X_train)
+            train_rmse = np.sqrt(loss_fn(y_pred, y_train))
+            y_pred = model(X_test)
+            test_rmse = np.sqrt(loss_fn(y_pred, y_test))
+        print("Epoch %d: train RMSE %.4f, test RMSE %.4f" % (epoch, train_rmse, test_rmse))
+
+    with torch.no_grad():
+        # shift train predictions for plotting
+        train_plot = np.ones_like(ts) * np.nan
+        y_pred = model(X_train)
+        y_pred = y_pred[:, -1, :]
+        train_plot[lookback:train_size] = model(X_train)[:, -1, :]
+        # shift test predictions for plotting
+        test_plot = np.ones_like(ts) * np.nan
+        test_plot[train_size + lookback:len(ts)] = model(X_test)[:, -1, :]
+
+    # plot
+    plt.plot(df)
+    plt.plot(train_plot, c='r')
+    plt.plot(test_plot, c='g')
+    plt.show()
 
 
 
@@ -50,6 +138,7 @@ def main(stockList):
 if __name__ == '__main__':
     warnings.simplefilter(action='ignore', category=FutureWarning)
     stockList = ['AAPL','IBM']
-    main(stockList)
+    # single_stock(stockList)
+    forecast()
 
 
--- model/LSTM.py
+++ model/LSTM.py
@@ -7,23 +7,31 @@ import torch.optim as optim
 
 torch.manual_seed(1)
 
-class LSTMTagger(nn.Module):
-    def __init__(self, embedding_dim, hidden_dim, vocab_size, tagset_size):
-        super(LSTMTagger, self).__init__()
-        self.hidden_dim = hidden_dim
+class StockLSTM(nn.Module):
+    def __init__(self):
+        # , embedding_dim, hidden_dim, vocab_size, tagset_size
+        super().__init__()
+        self.lstm = nn.LSTM(input_size = 1, hidden_size = 50, num_layers = 1, batch_first = True)
+        self.linear = nn.Linear(50,1)
 
-        self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
+        #self.hidden_dim = hidden_dim
+
+        #self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
 
         # The LSTM takes word embeddings as inputs, and outputs hidden states
         # with dimensionality hidden_dim.
-        self.lstm = nn.LSTM(embedding_dim, hidden_dim)
+        #self.lstm = nn.LSTM(embedding_dim, hidden_dim)
 
         # The linear layer that maps from hidden state space to tag space
-        self.hidden2tag = nn.Linear(hidden_dim, tagset_size)
+        #self.hidden2tag = nn.Linear(hidden_dim, tagset_size)
 
-    def forward(self, sentence):
+    def forward(self, x):
+        '''
         embeds = self.word_embeddings(sentence)
         lstm_out, _ = self.lstm(embeds.view(len(sentence), 1, -1))
         tag_space = self.hidden2tag(lstm_out.view(len(sentence), -1))
         tag_scores = F.log_softmax(tag_space, dim=1)
-        return tag_scores
+        '''
+        x,_ = self.lstm(x)
+        x = self.linear(x)
+        return x
